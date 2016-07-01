8a3ee3c9d3d7135aed2c7a3f2f6e4477f71b69bd
Pablo FM <pafloma@gmail.com>
2016-07-01 19:14:45 +0200

    Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn cuiquer.wsgi --log-file -
--- cuiquer/settings.py
+++ cuiquer/settings.py
@@ -129,3 +129,8 @@ STATICFILES_FINDERS = (
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'landing/static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
