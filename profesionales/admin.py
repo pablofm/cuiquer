de3f687d9b6ab293b0daa3c8100ab1af873d629c
Pablo FM <pafloma@gmail.com>
2016-10-06 12:17:44 +0200

    añadidiso campos al listado

--- profesionales/admin.py
+++ profesionales/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from profesionales.models import Profesional, Servicio, Categoria
-from perfiles.models import Usuario
 
 
 class ProfesionalAdmin(admin.ModelAdmin):
-    list_display = ('usuario__nombre', 'usuario__email')
+    list_display = ('usuario__nombre', 'usuario__email', 'usuario__telefono', 'codigo_postal')
     list_filter = ['servicios']
 
     def usuario__nombre(self, obj):
@@ -12,6 +11,9 @@ class ProfesionalAdmin(admin.ModelAdmin):
 
     def usuario__email(self, obj):
         return obj.usuario.email
+
+    def usuario__telefono(self, obj):
+        return obj.usuario.telefono
 
 admin.site.register(Profesional, ProfesionalAdmin)
 admin.site.register(Servicio)
