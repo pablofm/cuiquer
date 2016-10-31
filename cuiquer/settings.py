98329c06806edd1f29e0461838d46487ff3377be
Pablo FM <pafloma@gmail.com>
2016-10-31 21:32:16 +0100

    emails configurados

--- cuiquer/settings.py
+++ cuiquer/settings.py
@@ -148,14 +148,7 @@ class Dev(Common):
             'NAME': os.path.join(Common.BASE_DIR, 'db.sqlite3'),
         }
     }
-    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'mail.privateemail.com'
-    EMAIL_PORT = 465
-    EMAIL_HOST_USER = 'hello@cuiquer.com'
-    EMAIL_HOST_PASSWORD = 'cuiquer123'
-    EMAIL_USE_SSL = True
-    EMAIL_USE_TLS = False
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 class Prod(Common):
@@ -172,7 +165,8 @@ class Prod(Common):
     EMAIL_PORT = 465
     EMAIL_HOST_USER = 'hello@cuiquer.com'
     EMAIL_HOST_PASSWORD = 'cuiquer123'
-    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = False
 
     ROLLBAR = {
         'access_token': 'b5781909e8464502bade1b1127406ae1',
